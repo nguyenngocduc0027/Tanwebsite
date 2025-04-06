4a86bb3fc9ab1b240ec771995fed86a4a84889cc
Nguyen Ngoc Duc <nguyenngocduc.tosa@gmail.com>
2025-04-06 22:19:10 +0700

    new

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -75,6 +75,16 @@
     @include('layouts.footer')
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <!-- Custom JS -->
+    <script src="/assets/js/main.js"></script>
+    <script src="/assets/js/index.js"></script>
+    <script src="/assets/js/quickview.js"></script>
+    <script src="/assets/js/jquery.js"></script>
+    <script src="/assets/js/lazy.js"></script>
+    <script src="/assets/js/starts.min.js"></script>
+    <script src="/assets/js/swiper.js"></script>
 
 </body>
 
